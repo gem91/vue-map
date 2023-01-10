79a47ef5042199b5d6e7f76bb92d87f45d1f68ca
김민정 <mjkim@ipartners.co.kr>
2023-01-10 18:06:31 +0900

    tab 메뉴 컴포넌트 제작

---
+++ src/components/element/tab/TabContents.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tab_cont">
+    <ul class="tab_cont_list">
+      <li v-for="(data, idx) in tabContData" :key="idx">
+        <a href="#" class="thumbs">
+          <img :src="data.store.img" :alt="data.store.alt">
+        </a>
+        <div class="text_wrap">
+          <h4 class="title">{{ idx + 1 }}_ {{ data.store.name }}</h4>
+          <p class="address">{{ data.store.address }}</p>
+          <div class="review">
+            <span class="profile">
+              <img :src="data.profile.img" :alt="`${data.profile.name} 프로필 사진`">
+            </span>
+            <div class="comment">
+              <em class="name">{{ data.profile.name }}'s</em>
+                {{ data.profile.comment }}
+            </div>
+          </div>
+        </div>
+        <button class="btn-more">{{data.store.name}} 더보기</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    tabContData: {
+      type: Object,
+      default: null
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import './TabContents.scss';
+</style>
---
+++ src/components/element/tab/TabMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="tab_menus">
+    <li v-for="menu in tabMenuData" :key="menu.name" :class="{on : menu.isOn}">
+      <button ># {{ menu.name }}</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:{
+    tabMenuData: {
+      type: Object,
+      default: null
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .tab_menus{
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    li{
+      margin-right: 15px;
+      padding: 5px 20px 7px;
+      border-radius: 15px;
+      background: #aaa;
+      transition: .5s ease-in-out;
+      &:last-child{
+        margin-right: 0;
+      }
+      &.on, &:hover{
+        background-color: #ff7100;
+      }
+      button{
+        color: $text-color-white;
+      }
+    }
+  }
+</style>
