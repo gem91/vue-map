30ac71e705a6e8d97569c46234d1cb9109d29e61
김민정 <mjkim@ipartners.co.kr>
2023-01-10 18:04:18 +0900

    header sns

---
+++ src/components/element/sns/SnsLists.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul class="sns">
+      <li v-for="item in sns" :key="item.name">
+        <router-link :to="item.href">
+          {{ item.name }}
+        </router-link>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sns: [
+        {
+          name: "인스타그램",
+          href: "/insta",
+        },
+        {
+          name: "카카오톡",
+          href: "/kakao"
+        },
+      ]
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import './SnsLists.scss';
+</style>
