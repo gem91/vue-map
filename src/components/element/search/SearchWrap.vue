75f3d0f1859c762ed53817f1c96dfb92de381dac
mjkim <mjkim@ipartners.co.kr>
2023-01-11 01:14:06 +0900

    search 컴포넌트 v-mdoel 설정

--- src/components/element/search/SearchWrap.vue
+++ src/components/element/search/SearchWrap.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="seach_wrap">
-    <input type="text" placeholder="검색어를 입력하세요">
-    <button class="btn_search">검색</button>
+    <input 
+    type="text" 
+    placeholder="검색어를 입력하세요"
+    v-model="msg"
+    @keyup.enter="getSearchText"
+    >
+    <button class="btn_search" @click="getSearchText">검색</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'VueMapSearchWrap',
-
-  data() {
-    return {
-      
-    };
+  data(){
+    return{
+      msg: ''
+    }
+  },
+ methods: {
+    getSearchText(){
+      alert(this.msg)
+      this.msg = ''
+    }
   },
 };
 </script>
