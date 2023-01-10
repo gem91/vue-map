5ebea265c461b8d78faee6d6741e1fdb2015691f
김민정 <mjkim@ipartners.co.kr>
2023-01-10 18:05:03 +0900

    맛지도 배너컴포넌트

---
+++ src/components/element/banner/BannerWrap.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="banner_wrap">
+    <img :src="`${publicPath}images/${imgSrc}`">
+    <div class="container">
+      <h2>{{ title }}</h2>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+   return {
+      publicPath: process.env.BASE_URL
+    }
+  },
+
+  props:{
+    title: {
+        type: String,
+        default: '배너문구 고민 중'
+      },
+    imgSrc: {
+      type: String,
+      default: null
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import './BannerWrap.scss';
+</style>
---
+++ src/components/element/search/SearchWrap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="seach_wrap">
+    <input type="text" placeholder="검색어를 입력하세요">
+    <button class="btn_search">검색</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueMapSearchWrap',
+
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
